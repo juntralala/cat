d4b80188ecae696f1c7cbfeceb0e0695e5f41938
juntralala <ujun.muhammadjunaidi@gmail.com>
2025-11-19 12:26:31 +0800

    chore: istalled vuetify

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,8 +4,11 @@ import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createApp, h } from 'vue';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+import {createVuetify} from 'vuetify';
+import 'vuetify/styles';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+const vuetify = createVuetify();
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
@@ -13,6 +16,7 @@ createInertiaApp({
     setup({ el, App, props, plugin }) {
         createApp({ render: () => h(App, props) })
             .use(plugin)
+            .use(vuetify)
             .use(ZiggyVue)
             .mount(el);
     },
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import vue from '@vitejs/plugin-vue';
-import autoprefixer from 'autoprefixer';
 import laravel from 'laravel-vite-plugin';
 import path from 'path';
-import tailwindcss from 'tailwindcss';
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from 'vite';
+import vuetify from 'vite-plugin-vuetify';
 
 export default defineConfig({
     plugins: [
@@ -19,15 +19,12 @@ export default defineConfig({
                 },
             },
         }),
+        vuetify({autoImport: true}),
+        tailwindcss(),
     ],
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './resources/js'),
-        },
-    },
-    css: {
-        postcss: {
-            plugins: [tailwindcss, autoprefixer],
         },
     },
 });
